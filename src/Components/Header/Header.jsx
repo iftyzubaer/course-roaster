d07c47fc35a334e82018efe4f0defbe6b5087b43
iftyzubaer <iftyzubaer1304.com>
2023-09-15 21:45:19 +0600

    created course.json with generated data and added the header of the webpage

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='mt-12 mb-8 text-center text-3xl font-bold'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
